48687e8775dc2fac25b81c2656c6582dc33a841d
Sakera Begum <sakerasiu23@gmail.com>
2026-09-09 22:16:50 -0400

    Expose calibration and authoritative public-data APIs

--- src/equitable_capital/__init__.py
+++ src/equitable_capital/__init__.py
@@ -6,6 +6,33 @@ from .benchmark import (
     run_model_benchmark,
     summarize_benchmark,
 )
+from .calibration import (
+    calibration_curve_table,
+    expected_calibration_error,
+    run_calibration_benchmark,
+    summarize_calibration,
+)
+from .cdfi import (
+    CDFI_CERTIFICATION,
+    load_cdfi_certification_workbook,
+    summarize_cdfi_by_state,
+)
+from .census_abs import (
+    ABS_2022_COMPANY_SUMMARY,
+    build_abs_query_url,
+    load_abs_county_data,
+    load_abs_state_data,
+    total_population_rows,
+)
+from .census_cbp import (
+    CENSUS_CBP_2023,
+    STATE_ABBR_TO_FIPS,
+    county_name_lookup,
+    load_cbp_county_file,
+    load_county_geojson,
+    summarize_cbp_county_totals,
+    summarize_county_industry_concentration,
+)
 from .data import generate_synthetic_startups
 from .explainability import explain_applicant
 from .fairness import fairness_audit, opportunity_gap
@@ -23,23 +50,42 @@ from .modeling import (
 from .public_data import PUBLIC_DATASETS, get_sba_dataset_metadata, load_sba_public_workbook
 
 __all__ = [
+    "ABS_2022_COMPANY_SUMMARY",
+    "CDFI_CERTIFICATION",
+    "CENSUS_CBP_2023",
     "ModelResult",
     "PUBLIC_DATASETS",
+    "STATE_ABBR_TO_FIPS",
     "allocate_capital",
     "benchmark_model_registry",
+    "build_abs_query_url",
     "build_pipeline",
+    "calibration_curve_table",
+    "county_name_lookup",
+    "expected_calibration_error",
     "explain_applicant",
     "fairness_audit",
     "generate_synthetic_startups",
     "get_sba_dataset_metadata",
     "global_feature_importance",
+    "load_abs_county_data",
+    "load_abs_state_data",
+    "load_cbp_county_file",
+    "load_cdfi_certification_workbook",
+    "load_county_geojson",
     "load_sba_public_workbook",
     "opportunity_gap",
     "prepare_public_state_map",
     "public_state_metric_options",
+    "run_calibration_benchmark",
     "run_model_benchmark",
     "summarize_allocation",
-    "summarize_synthetic_states",
     "summarize_benchmark",
+    "summarize_calibration",
+    "summarize_cbp_county_totals",
+    "summarize_cdfi_by_state",
+    "summarize_county_industry_concentration",
+    "summarize_synthetic_states",
+    "total_population_rows",
     "train_model",
 ]
